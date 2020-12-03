bc93cfe5746a80689172e4f1edea398b2393587d
ange47rm <ange47rm@hotmail.com>
2020-12-03 10:23:03 +0000

    Added note

--- homework-flask-app/controllers/controller.py
+++ homework-flask-app/controllers/controller.py
@@ -17,3 +17,11 @@ def browser_muliply(num1, num2):
 @app.route ('/divide/<num1>/<num2>')
 def browser_divide(num1, num2):
     return divide(num1, num2)
+
+# THE ABOVE WORKS HOWEVER THE DECORATORS ARE NOT REALLY "DECORATING", I SHOULD HAVE LET THE 
+# DECORATOR ADD THE STRING OF TEXT, AND THE CALCULATOR SHOULD HAVE JUST RETURNED A NUMBER.
+
+# decorator: decorator is a function that takes another function as a parameter and extends 
+# the behavior of the latter function without explicitly modifying it. Once the decorator 
+# function is defined, we can use the @ symbol along with the name of the decorator function and 
+# place it above the definition of the function to be decorated.
